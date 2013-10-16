627a5bd20f0f722f7f7031e6da60fbab5fdb4b42
Allen Chun <allen.chun@hotmail.com>
2013-10-16 16:56:08 +0800

    Fixes on twitter

--- jobs/twitter.rb
+++ jobs/twitter.rb
@@ -1,16 +1,14 @@
 require 'twitter'
 
-
 #### Get your twitter keys & secrets:
 #### https://dev.twitter.com/docs/auth/tokens-devtwittercom
 Twitter.configure do |config|
-  config.consumer_key = 'jl46ztnyjSF91RHg2lrog'
-  config.consumer_secret = '8yOwzT2i51SZXrmAo2iixvJ3QN9Gnwtih7jK1yVONU'
-  config.oauth_token = '1130138335-EMyDiIC7O8MbFJB7N4FvasHjyS9nD7nZ0dA80US'
-  config.oauth_token_secret = '7owogLjQKBNu6xRSwx1IZtXHzPTCb51Hw2weUdVTg'
+  config.consumer_key = 'OofUMsxCiT8dvh49VSvQmA'
+  config.consumer_secret = 'eq725PM1vI1hhYNOsUgUFsrCt4TnXI6UOr1hpRPpGo'
+  config.oauth_token = '284385197-pmYyZiD74yHiJ85tZfWCmNHyxMTiolrs0q94M6au'
+  config.oauth_token_secret = 'Z75dp5OtyG3LA83X09SGyLsv57Sozx7GO4Oi5ZzCLrA'
 end
 
-#search_term = URI::encode('#todayilearned')
 user = "BangkoSentral"
 
 SCHEDULER.every '10m', :first_in => 0 do |job|
@@ -22,7 +20,7 @@ SCHEDULER.every '10m', :first_in => 0 do |job|
       tweets.map! do |tweet|
         { name: tweet.user.name, body: tweet.text, avatar: tweet.user.profile_image_url_https }
       end
-      send_event('twitter_mentions', comments: tweets)
+      send_event('twitter_feeds', comments: tweets)
     end
   rescue Twitter::Error
     puts "\e[33mFor the twitter widget to work, you need to put in your twitter API keys in the jobs/twitter.rb file.\e[0m"
