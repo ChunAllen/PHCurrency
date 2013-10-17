d9edcc485759de697765e4dd534b00ae145ad43d
Allen Chun <allen.chun@hotmail.com>
2013-10-17 23:40:13 +0800

    fixes

--- jobs/forex.rb
+++ jobs/forex.rb
@@ -1,24 +1,22 @@
 require 'rest-client'
 require 'json'
 
-URL = "http://phisix-api.appspot.com/stocks.json"
+URL = "http://rate-exchange.appspot.com/currency?from=JPY&to=PHP"
 
 SCHEDULER.every '2s', :first_in => 0 do |job|
   value = RestClient.get URL
-  data = JSON.parse(value)["stock"]
+  data = JSON.parse(value)
 
   #values
-  stocks = Forex.new data
-  values = stocks.value(settings.stock_symbol)
+  #values = stocks.value(settings.stock_symbol)
 
   #top advances
-  advances = stocks.top_changes.map{|stock| { :label => stock["name"],
-                                              :value => stock["percent_change"]}}
+  advances = data.map{|forx| { :label => forx["from"],
+                                              :value => forx["rate"]}}
 
-
-  send_event('stock', {current: values[:current], last: values[:previous]})
-  send_event('stock-volume', {value: stocks.find(settings.stock_symbol)["volume"],
-                              max: stocks.total_volume})
+  #send_event('stock', {current: values[:current], last: values[:previous]})
+  #send_event('stock-volume', {value: stocks.find(settings.stock_symbol)["volume"],
+                              #max: stocks.total_volume})
   send_event('advances', {items: advances})
 end
 
